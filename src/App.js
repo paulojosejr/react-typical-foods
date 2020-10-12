c2221ab04878c91058df80300158998adb99bcb9
Paulo Araújo <paulojosejr95@gmail.com>
2020-10-12 10:58:46 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Navbar from './components/Navbar/Navbar'
+import './App.scss'
+
+
+class App extends Component {
+  render() {
+    return <Navbar />
+  }
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Navbar.scss'
+import  logo from '../../logo.png'
+
+export default function Navbar() {
+    return <nav className="navbar">
+        <img src= {logo} alt='food tours logo' />
+    </nav>
+
+}
