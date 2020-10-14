83b42b52b1509d0d9f2d96126912cc22c4d4bb07
Paulo Araújo <paulojosejr95@gmail.com>
2020-10-14 17:34:14 +0100

    data and images

--- src/components/FoodList/index.js
+++ src/components/FoodList/index.js
@@ -3,6 +3,9 @@ import './FoodList.scss';
 import Food from '../Food/Food'
 
 class FoodList extends Component {
+    state={
+        foods:[]
+    }
     render() {
         return (
             <section className="foodlist">
---
+++ src/components/foodData.js
@@ -0,0 +1,34 @@
+export const tourData = [
+  {
+    id: 1,
+    food: "francesinha",
+    img: "./img/francesinha.jpg",
+    name: "francesinha",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 2,
+    food: "Pastel de nata",
+    img: "./img/natas.jpg",
+    name: "pastel de nata",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 3,
+    food: "bifana",
+    img: "./img/bifana.jpg",
+    name: "bifana",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  },
+  {
+    id: 4,
+    food: "bacalhau",
+    img: "./img/bacalhau.jpg",
+    name: "bacalhau",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel,repellendus!"
+  }
+];
