4eb7586d36ef1499a57ba56206a583eef3fb1da8
Paulo Araújo <paulojosejr95@gmail.com>
2021-07-20 11:37:02 +0100

    add footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+import './Footer.scss'
+import React from 'react'
+
+export default props =>
+    <footer className="footer">
+        <span>
+            Developed with <i className="fa fa-heart text-danger"></i> by <strong> Paulo Araújo</strong>
+        </span>
+    </footer>
